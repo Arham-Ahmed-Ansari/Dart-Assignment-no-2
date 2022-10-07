addb7836a0d5b41a03c1e0c20fce84d06a5c387f
Arham-Ahmed-Ansari <arhamahmedansari92@gmail.com>
2022-10-07 19:49:44 +0500

    Assignment Completed

---
+++ main.dart
@@ -0,0 +1,158 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // Question no 1
+  // int length = 4;
+  // int breadth = 4;
+  // if (length == breadth) {
+  //   print("The values are of square ");
+  // } else {
+  //   print("The values are of rectangle");
+  // }
+
+  // Question no 2
+
+  // print("Enter first person age:");
+  // int? Firstperson = int.parse(stdin.readLineSync()!);
+
+  // print("Enter Second person age:");
+  // int? Secondperson = int.parse(stdin.readLineSync()!);
+
+  // print("Enter Third person age:");
+  // int? Thirdperson = int.parse(stdin.readLineSync()!);
+
+  // if ((Firstperson > Secondperson) && (Firstperson > Thirdperson)) {
+  //   print("The first persom is oldest");
+  // }
+  // if ((Firstperson < Secondperson) && (Firstperson < Thirdperson)) {
+  //   print("The first person is yongest");
+  // }
+  // if ((Secondperson > Firstperson) && (Secondperson > Thirdperson)) {
+  //   print("The Second person is oldest");
+  // }
+  // if ((Secondperson < Firstperson) && (Secondperson < Thirdperson)) {
+  //   print("The Second person is youngest");
+  // }
+  // if ((Thirdperson > Firstperson) && (Thirdperson > Secondperson)) {
+  //   print("The third person is oldest");
+  // }
+  // if ((Thirdperson < Firstperson) && (Thirdperson < Secondperson)) {
+  //   print("The third person is youngest");
+  // }
+
+  // Question no 3
+
+  // int Classes_held = 16;
+  // int Classes_attendent = 10;
+  // double Attendance_percentage = Classes_attendent / Classes_held * 100;
+  // print("Class Attendent percentage $Attendance_percentage");
+  // if (Attendance_percentage > 75) {
+  //   print("Student is allowed to sit in class");
+  // } else {
+  //   print("Student is not allowed to sit in class");
+  // }
+
+  // Question no 4
+
+  // print("Enter any year:");
+  // int year = int.parse(stdin.readLineSync()!);
+
+  // if ((year % 4 == 0) || (year % 400 == 0)) {
+  //   print("The year is a leap year");
+  // } else {
+  //   print("This is not a leap year");
+  // }
+
+  // Question no 5
+
+  // num Temperature = 42;
+  // if (Temperature == 0) {
+  //   print("Freezinf Weather");
+  // } else if ((Temperature == 0) ||
+  //     (Temperature == 1) ||
+  //     (Temperature == 2) ||
+  //     (Temperature == 3) ||
+  //     (Temperature == 4) ||
+  //     (Temperature == 5) ||
+  //     (Temperature == 6) ||
+  //     (Temperature == 7) ||
+  //     (Temperature == 8) ||
+  //     (Temperature == 9) ||
+  //     (Temperature == 10)) {
+  //   print("Very cold Weather");
+  // } else if ((Temperature == 11) ||
+  //     (Temperature == 12) ||
+  //     (Temperature == 13) ||
+  //     (Temperature == 14) ||
+  //     (Temperature == 15) ||
+  //     (Temperature == 16) ||
+  //     (Temperature == 17) ||
+  //     (Temperature == 18) ||
+  //     (Temperature == 19) ||
+  //     (Temperature == 20)) {
+  //   print("cold Weather");
+  // } else if ((Temperature == 21) ||
+  //     (Temperature == 22) ||
+  //     (Temperature == 23) ||
+  //     (Temperature == 24) ||
+  //     (Temperature == 25) ||
+  //     (Temperature == 26) ||
+  //     (Temperature == 27) ||
+  //     (Temperature == 28) ||
+  //     (Temperature == 29) ||
+  //     (Temperature == 30)) {
+  //   print("Normal in temperature");
+  // } else if ((Temperature == 31) ||
+  //     (Temperature == 32) ||
+  //     (Temperature == 33) ||
+  //     (Temperature == 34) ||
+  //     (Temperature == 35) ||
+  //     (Temperature == 36) ||
+  //     (Temperature == 37) ||
+  //     (Temperature == 38) ||
+  //     (Temperature == 39) ||
+  //     (Temperature == 40)) {
+  //   print("weather is hot");
+  // } else {
+  //   print("Very hot weather");
+  // }
+
+  // Question no 6
+
+  // print("Enter any alphabet");
+  // String? alphabet = stdin.readLineSync();
+  // if (((alphabet == "a") ||
+  //         (alphabet == "e") ||
+  //         (alphabet == "i") ||
+  //         (alphabet == "o") ||
+  //         (alphabet == "u")) ||
+  //     (alphabet == "A") ||
+  //     (alphabet == "E") ||
+  //     (alphabet == "I") ||
+  //     (alphabet == "O") ||
+  //     (alphabet == "U")) {
+  //   print("The Enter Alphabet is Vovel");
+  // } else {
+  //   print("The enter alphabet is constant");
+  // }
+
+  //Question no 7
+
+  // int id = 1001;
+  // String name = "james";
+  // double unit = 800;
+  // double net_Bill_amount = unit + 1040;
+  // print("Customer Id number: ${id}");
+  // print("Customer name: ${name}");
+  // print("Unit consumed: ${unit}");
+  // if (unit <= 199) {
+  //   print("Amount charges @ 1.20 per unit: ${unit * 1.20}");
+  // } else if ((unit >= 200) && (unit <= 400)) {
+  //   print("Amount charges @ 1.50 per unit: ${unit * 1.50}");
+  // } else if ((unit >= 400) && (unit <= 600)) {
+  //   print("Amount charges @ 1.80 per unit: ${unit * 1.80}");
+  // } else if (unit >= 600) {
+  //   print("Amount charges @ 2.00 per unit: ${unit * 2.00}");
+  // }
+  // print("net bill amount:$net_Bill_amount");
+}
